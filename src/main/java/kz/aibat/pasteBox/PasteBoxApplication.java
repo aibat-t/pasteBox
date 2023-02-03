6dbf14b87455cef53d45aa55899e567fb9440a86
Aibat Tozhybayev <tozhybayev.aibat@gmailcom>
2023-02-03 21:04:51 +0600

    Init

---
+++ src/main/java/kz/aibat/pasteBox/PasteBoxApplication.java
@@ -0,0 +1,13 @@
+package kz.aibat.pasteBox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasteBoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasteBoxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kz/aibat/pasteBox/PasteBoxApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.aibat.pasteBox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasteBoxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
